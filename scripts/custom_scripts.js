79f8f5a35ce4c50062ef51453f660aa4e5501e3e
Bartek <buczek.bartosz@wp.pl>
2022-10-06 12:14:26 +0200

    caroules slides change on finger swipe

--- scripts/custom_scripts.js
+++ scripts/custom_scripts.js
@@ -1,6 +1,10 @@
 var bookmarks = document.querySelectorAll("#main-content .bookmarks .bookmark");
 var carousel_articles = document.querySelector("#blog .carousel .articles");
-var carousel_control = document.querySelectorAll("#blog .carousel .control .circle");
+var carousel_control = document.querySelectorAll(
+  "#blog .carousel .control .circle"
+);
+var current_slide = 0;
+var blog = document.querySelector("#blog");
 
 function show_bookmark(n) {
   open_elem(bookmarks[n - 1], 500);
@@ -13,21 +17,42 @@ function close_bookmark() {
 }
 
 async function set_active_slide(n) {
-  n--;
-  if(window.matchMedia("(max-width: 767px)").matches) {
-    carousel_articles.setAttribute(
-    "style",
-    "transform: translateX(-" + (n * 100) + "vw);"
-  );
-  } else if(window.matchMedia("(min-width: 992px)").matches) {
+  if (window.matchMedia("(max-width: 767px)").matches) {
     carousel_articles.setAttribute(
       "style",
-      "transform: translateX(-" + (n * 50) + "vw);"
+      "transform: translateX(-" + n * 100 + "vw);"
     );
+  } else if (window.matchMedia("(min-width: 992px)").matches) {
+    carousel_articles.setAttribute(
+      "style",
+      "transform: translateX(-" + n * 50 + "vw);"
+    );
+  } else {
   }
-  else {}
-  
-  for (var i = 0; i < carousel_control.length; i++)
-    carousel_control[i].classList.remove("active");
+  carousel_control[current_slide].classList.remove("active");
   carousel_control[n].classList.add("active");
+  current_slide = n;
+}
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+
+function handleTouchStart(evt) {
+  const firstTouch = (evt.touches || evt.originalEvent.touches)[0];
+  xDown = firstTouch.clientX;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !evt.path.includes(blog)) return;
+
+  var xDiff = xDown - evt.touches[0].clientX;
+
+  if (xDiff > 0 && current_slide < carousel_articles.children.length - 1) {
+    set_active_slide(current_slide + 1);
+  } else if (xDiff < 0 && current_slide > 0) {
+    set_active_slide(current_slide - 1);
+  }
+  xDown = null;
 }
